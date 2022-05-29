81a1ba720f26bfcbf39b02dcf815345934aa3596
Wagner Braga <wfbraga@hotmail.com>
2022-05-29 18:28:22 -0400

    creating test for RecurringBill model test covers model validations for all fields

--- app/models/recurring_bill.rb
+++ app/models/recurring_bill.rb
@@ -1,3 +1,9 @@
 class RecurringBill < ApplicationRecord
   belongs_to :group
+  validates :description, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0.0 }
+  validates :active, presence: true
+  validates :active, presence: true
+  validates :due_day, presence: true, numericality: { greater_than: 0, lower_than: 31 }
+  validates :group_id, presence: true
 end
--- spec/models/recurring_bill_spec.rb
+++ spec/models/recurring_bill_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe RecurringBill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recurring_bills) { RecurringBill.new(
+                            description: 'Test description',
+                            amount: 100.75,
+                            active: :active,
+                            due_day: 5,
+                            group_id: 1
+                          ) }
+  context 'When creating a invalid group' do
+    it 'description can not be empty.' do
+      recurring_bills.description = ''
+      expect(recurring_bills).to be_invalid
+    end
+    it 'amount can not be empty.' do
+      recurring_bills.amount = nil
+      expect(recurring_bills).to be_invalid
+    end
+    it 'amount can not be equal or smaller than zero.' do
+      recurring_bills.amount = 0
+      expect(recurring_bills).to be_invalid
+    end
+    it 'active can not be empty' do
+      recurring_bills.active = nil
+      expect(recurring_bills).to be_invalid
+    end
+    it 'due_day can not be empty' do
+      recurring_bills.due_day = nil
+      expect(recurring_bills).to be_invalid
+    end
+    it 'due_day can not be equal or smaller than zero or greater than 31.' do
+      recurring_bills.due_day = 0
+      expect(recurring_bills).to be_invalid
+    end
+    it 'groud_id can not be empty' do
+      recurring_bills.group_id = nil
+      expect(recurring_bills).to be_invalid
+    end
+  end
 end
