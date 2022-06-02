38368393786a3433f0dbe23ae568d6daa25af679
Wagner Braga <wfbraga@hotmail.com>
2022-06-02 08:09:26 -0400

    I've have tested your suggestion and it worked as you shown. Also, reading the code i  belongs_to :user, foreign_key: 'owner_id'   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id't seamed out of place. I've decided commit the changes direct from you suggestion. Changes on app/models/group.rb goes: From: `belongs_to :user, foreign_key: 'owner_id'` to: `belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'`

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
 
-  validates :name, :owner_id, presence: true
+  validates :name, presence: true
 end
