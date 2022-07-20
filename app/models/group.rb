6c928ae0d2d3c71fa415d96d58c0ba2b86fc452f
Wagner Braga <wfbraga@hotmail.com>
2022-07-20 08:22:38 -0400

    creating relation between Groups, InstallmentPurchase and RecurringBill as per Trello Task: https://trello.com/c/wc7a41FH/8-criar-relacionamento-entre-group-e-installmentpurchase-recurringbill
    Factory Creation for InstallmentPurchase
    Test refactoring for InstallmentPurchase
    Minor improvements on Group model and fatory

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  has_many :installment_purchases
+  has_many :recurring_bills
 
   validates :name, presence: true
-  validates :owner_id, presence: true
 end
