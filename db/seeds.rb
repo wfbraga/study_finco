79b84ad03827e037b54c7620beb0d7b6fc1e67f7
Wagner Braga <wfbraga@hotmail.com>
2022-08-09 08:06:07 -0400

    Creating model Operation as per Trello task (#11)
    * Creating model Operation as per Trello task
    https://trello.com/c/452lEkGe/11-criar-modelo-operation
    
    Creating model Operation and its relations
    Creation of factory and test
    Update of seed
    Update of model Account and its tests to reflect the new relation
    Minor improvement on group test.
    
    * Creating model AccountDebit as per Trello task:
    https://trello.com/c/MKLpZrjA/12-criar-modelo-accountdebit
    
    Ceation of model AccountDebit, factory and tests
    Upudating model User to refect new relation
    
    * Creating model AccountCreditCard as per Trello task (#12)
    
    * Creating model AccountCreditCard as per Trello task:
    https://trello.com/c/sty2QrdD/13-criar-modelo-accountcreditcard
    
    Creating model AccountCreditCard and its relations
    Creation of factory and test
    Update of seed
    Update of model User and ists test to reflect the new relation
    
    * Creating model AccountObjective as per Trello tas: (#13)
    
    https://trello.com/c/YAbFGuRf/14-criar-modelo-accountobjective
    
    Creatin model AccountObjective and its relations
    Creation of factory and test
    Update of Seed
    Update of model User and its test to reflec the new relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+print 'Seeding the Bank'
+
 (1..50).each do |id|
   User.create(
     name: Faker::Name.name,
@@ -52,4 +54,45 @@
     balance: rand(0..1000),
     owner_id: rand(1..id)
   )
+
+  Operation.create(
+    description: Faker::Lorem.paragraph,
+    kind: %i[debit credit].sample,
+    account_id: rand(1..id),
+    amount: rand(1..100_000)
+  )
+
+  account = Account.find_by(id: rand(1..id))
+  AccountDebit.create(
+    bank: Faker::Company.name,
+    branch: rand(999..10_000).to_s,
+    number: "#{rand(11_000..100_000)}-#{%i[a f g h t y].sample}",
+    credit_limit: rand(0..10_000),
+    account_id: account.id,
+    owner_name: account.owner.name,
+    owner_id: account.owner.id
+  )
+
+  AccountCreditCard.create(
+    bank: Faker::Company.name,
+    brand: Faker::Commerce.brand,
+    last_digits: rand(1000..9999),
+    credit_limit: rand(500..10_000),
+    owner_name: account.owner.name,
+    due_day: rand(1..14),
+    expires_at: Time.zone.now + 1.year,
+    owner_id: account.owner.id,
+    account_id: account.id,
+  )
+
+  AccountObjective.create(
+    target_name: Faker::Commerce.product_name,
+    target_amount: Faker::Commerce.price,
+    acomplish_at: Time.zone.now + 1.year,
+    owner_name: account.owner.name,
+    owner_id: account.owner.id,
+    account_id: account.id
+  )
+  print '.'
 end
+print "!\nFinished succefuly!\n"
